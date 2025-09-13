2e8f89dfc8984dfc395c9b48fbbcdb9303aedd7c
LordYip <pranav.rao66@gmail.com>
2025-09-13 23:18:15 +0530

    Updated .gitignore to exclude .next directory

--- frontend/.next/static/chunks/src_app_tools_story_page_tsx_e50c6ffe._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_4dbb8ba2._.js",
-  "static/chunks/src_components_858484e7._.js"
-],
-    source: "dynamic"
-});
